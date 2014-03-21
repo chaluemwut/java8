6d89bdc600f0ead1d9832e7377275f0a7eefbb0d
chaluemwut <chaluemwut@hotmail.com>
2014-03-21 13:00:44 +0700

    init intellj

--- src/com/nsc/java8/TestMethod.java
+++ src/com/nsc/java8/TestMethod.java
@@ -1,5 +1,11 @@
 package com.nsc.java8;
 
+import sun.reflect.annotation.AnnotationParser;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class TestMethod {
 	
 	public static void main(String[] args) {
@@ -12,6 +18,12 @@ public class TestMethod {
 			}
 		};
 		d.test();
-	}
+
+        List<Integer> lst = Arrays.asList(1,2,6,8,9);
+        int arr[] = {2,3,4,5};
+        int sum = Arrays.stream(arr).map(n->n*0).sum();
+        System.out.println(sum);
+//        Stream.of(lst2).forEach(e->System.out.println(e));
+    }
 
 }
